9830ea2d8dc5a43ffd13e933c0e2d9052205b696
jang5714 <lionx3@naver.com>
2021-08-04 17:58:03 +0900

    0804

--- src/main/java/project/spring/jang/dish/KitchenService.java
+++ src/main/java/project/spring/jang/dish/KitchenService.java
@@ -1,8 +1,13 @@
 package project.spring.jang.dish;
 
 import reactor.core.publisher.Flux;
-
+/**
+ * Future 의 리턴타입은  Scalar 이다.
+ * Flux 의 리턴타입은 Vector 이다.
+ * just()를 통해 비동기적으로 제공한다.(만들자 마자...)
+ **/
 public class KitchenService {
+
     Flux<Dish> getDishes(){
         return Flux.just(
                 new Dish("Sesame chicken"),
--- src/main/java/project/spring/jang/dish/PoliteRestaurant.java
+++ src/main/java/project/spring/jang/dish/PoliteRestaurant.java
@@ -1,7 +1,10 @@
 package project.spring.jang.dish;
+/**
+* 구독(subscription) 을 하기 전에는 어떤 연산도 일어나지 않는다.
+**/
 
 public class PoliteRestaurant {
-    public static void main(String[] args) {
+    public static void main(String... args) {
         PoliteServer server = new PoliteServer(new KitchenService());
 
         server.doingMyjob().subscribe(
